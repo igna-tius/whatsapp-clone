ccb2fdaa365c4f2f6f3a33ac75506fe8821ba853
shubham dalal <dshubh2015@gmail.com>
2020-11-10 18:06:12 +0530

    added chat section

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,61 @@
-import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Chat.css";
+
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVert from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar />
+        <Avatar src="https://wallpapercave.com/wp/wp3978062.jpg" />
         <div className="chat__headerInfo">
           <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Shubham</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Shubham</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Shubham</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -5,7 +5,7 @@ import "./SidebarChat.css";
 function SidebarChat() {
   return (
     <div className="sidebarChat">
-      <Avatar />
+      <Avatar src="https://wallpapercave.com/wp/wp3978062.jpg" />
       <div className="sidebarChat__info">
         <h2>Room Name</h2>
         <p>This is the last message</p>
